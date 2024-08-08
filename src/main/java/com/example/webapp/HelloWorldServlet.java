f529bdc173df436391ae68cfcd871512a59e55b7
shashidhar-hexa <87765622+shashidhar-hexa@users.noreply.github.com>
2024-08-08 18:32:59 +0530

    Create HelloWorldServlet.java

---
+++ src/main/java/com/example/webapp/HelloWorldServlet.java
@@ -0,0 +1,19 @@
+package com.example.webapp;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = {"/hello"})
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello, World!</h1>");
+    }
+}
